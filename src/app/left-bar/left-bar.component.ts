4e34e76c0cb6d61db948d76a9933f997d1c5b12e
JimDepoorter <depoorterjim@hotmail.com>
2020-06-03 16:11:32 +0200

    5 files modified

--- src/app/left-bar/left-bar.component.ts
+++ src/app/left-bar/left-bar.component.ts
@@ -13,11 +13,13 @@ export interface Tile {
   styleUrls: ['./left-bar.component.css']
 })
 export class LeftBarComponent implements OnInit {
-  
+
   tiles: Tile[] = [
-    {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
+    {text: 'One', cols: 4, rows: 1, color: 'lightblue'},
     {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
     {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
+    {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
+    {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
     {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
   ];
 
